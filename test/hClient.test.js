10d06cb93985442de02aec4da1f73bb14a631c1c
Willem Olding <willemolding@gmail.com>
2019-03-06 11:57:25 +1100

    lintes tests

--- test/hClient.test.js
+++ test/hClient.test.js
@@ -1,101 +1,91 @@
-const hClient = require("../src/index.js")
-const keyManagement = require("../src/keyManagement.js")
+const hClient = require('../src/index.js')
+const keyManagement = require('../src/keyManagement.js')
 
-describe("hClient: basic test", () => {
-  
-  it("should be able to override window.holochainClient", async () => {
+describe('hClient: basic test', () => {
+  it('should be able to override window.holochainClient', async () => {
+    // mock holochainClient
+    const holochainClient = {
+      connect: (url) => new Promise((resolve) => {
+        console.log('connecting to ', url)
 
-  	// mock holochainClient
-  	const holochainClient = {
-  		connect: (url) => new Promise((resolve) => {
-  			console.log("connecting to ", url)
+        resolve({
+          call: (callString) => (params) => new Promise((resolve) => {
+            console.log(`${callString} was called with ${params}`)
+            resolve('original result')
+          })
+        })
+      })
+    }
 
-  			resolve({
-  				call: (callString) => (params) => new Promise((resolve) => {
-  					console.log(`${callString} was called with ${params}`);
-  					resolve("original result");
-  				})
-  			});
-  		})
-  	}
+    // make a call with the original mock
+    let firstCallResult
+    await holochainClient.connect('url1').then(({ call }) => {
+      call('callString1')('params1').then(result => {
+        console.log(result)
+        firstCallResult = result
+        firstCallResult.should.equal('original result')
+      })
+    })
 
-  	// make a call with the original mock
-  	let firstCallResult;
-  	await holochainClient.connect("url1").then(({call}) => {
-  		call("callString1")("params1").then(result => {
-  			console.log(result)
-  			firstCallResult = result
-  	    firstCallResult.should.equal("original result");
-      });
-    });
+    // use hClient to override
+    const url = 'ws://test'
+    const preCall = (callString, params) => ({ callString, params })
+    const postCall = response => 'override response'
+    const postConnect = ws => ws
+    const holoClient = hClient.makeWebClient(holochainClient, url, preCall, postCall, postConnect)
 
-
-  	// use hClient to override
-  	const url = "ws://test"
-  	const preCall = (callString, params) => ({callString, params});
-  	const postCall = response => "override response";
-  	const postConnect = ws => ws;
-  	const holoClient = hClient.makeWebClient(holochainClient, url, preCall, postCall, postConnect);
-
-	// make a call with the overriden version
-  	let secondCallResult;
-  	await holoClient.connect().then(({call}) => {
-  		call("callString1")("params1").then(result => {
-  			console.log(result)
-  			secondCallResult = result
-        secondCallResult.should.equal("override response");
-  		});
-  	});
-
-
+    // make a call with the overriden version
+    let secondCallResult
+    await holoClient.connect().then(({ call }) => {
+      call('callString1')('params1').then(result => {
+        console.log(result)
+        secondCallResult = result
+        secondCallResult.should.equal('override response')
+      })
+    })
   })
 })
 
-describe("keyManagement", function () {
+describe('keyManagement', function () {
+  this.timeout(5000)
 
-  this.timeout(5000);
+  it('Can get 32 bytes of local entropy from webcrypto or sodium as a fallback', async () => {
+    let entropy = await keyManagement.getLocalEntropy()
+    entropy.byteLength.should.equal(32)
+  })
 
-  it("Can get 32 bytes of local entropy from webcrypto or sodium as a fallback", async () => {
-    let entropy = await keyManagement.getLocalEntropy();
-    entropy.byteLength.should.equal(32);
-  });
-
-  it("Can generate a local readonly keypair", async () => {
-
+  it('Can generate a local readonly keypair', async () => {
     let keypair = await keyManagement.generateReadonlyKeypair(
       keyManagement.getLocalEntropy,
       keyManagement.getLocalEntropy
-    );
-    keypair._signPub.byteLength.should.equal(32);
-    keypair._signPriv.byteLength.should.equal(64);
-  });
+    )
+    keypair._signPub.byteLength.should.equal(32)
+    keypair._signPriv.byteLength.should.equal(64)
+  })
 
-  it("Can generate a new readwrite keypair", async () => {
-
-    const mockSaltRegistration = (email, salt) => salt;
+  it('Can generate a new readwrite keypair', async () => {
+    const mockSaltRegistration = (email, salt) => salt
 
     let keypair = await keyManagement.generateNewReadwriteKeypair(
-      "test@test.com",
-      "123abc",
+      'test@test.com',
+      '123abc',
       keyManagement.getLocalEntropy,
       keyManagement.getLocalEntropy,
       mockSaltRegistration
-    );
-    keypair._signPub.byteLength.should.equal(32);
-    keypair._signPriv.byteLength.should.equal(64);
-  });
+    )
+    keypair._signPub.byteLength.should.equal(32)
+    keypair._signPriv.byteLength.should.equal(64)
+  })
 
-  it("Can recover a keypair with an already registered salt", async () => {
-    const mockGetRegisteredSalt = (email) => keyManagement.getLocalEntropy();
+  it('Can recover a keypair with an already registered salt', async () => {
+    const mockGetRegisteredSalt = (email) => keyManagement.getLocalEntropy()
 
     let keypair = await keyManagement.regenerateReadwriteKeypair(
-      "test@test.com",
-      "123abc",
+      'test@test.com',
+      '123abc',
       mockGetRegisteredSalt
-    );
-    keypair._signPub.byteLength.should.equal(32);
-    keypair._signPriv.byteLength.should.equal(64);
-  });
-
+    )
+    keypair._signPub.byteLength.should.equal(32)
+    keypair._signPriv.byteLength.should.equal(64)
+  })
 })
-
