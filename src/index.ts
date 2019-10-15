8ae1b22402f9fb347346932778953d766279d2c4
Matthew Brisebois <matthew@webheroes.ca>
2019-10-15 22:43:58 +0000

    Update jsdoc comments o minor fixes o add more info to hApp class constructor

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,6 @@ interface DnaConfig {
     [x: string]: Array<string> | ZomeConfig,
 }
 
-enum ModeTypes {
-    Development,
-    Production,
-}
-
 const define_readonly = function ( obj, key, value ) {
     Object.defineProperty( obj, key, {
 	"value": value,
@@ -18,60 +13,106 @@ const define_readonly = function ( obj, key, value ) {
     });
 }
 
-
 /**
  * @module holo-host/web-sdk
  * 
  * @description
  * Full example
- * ```javascript
- * const HoloWebSDk = require("@holo-host/web-sdk");
- * 
+ * ```html
+ * <script type="text/javascript" src="./holo-web-sdk.js"></script>
+ * <script type="text/javascript">
  * (async () => {
  *     const happ = await HoloWebSDK.init();
+ *     await happ.signIn();
+ * 
  *     const holofuel = await happ.dna( "holofuel", {
  *         "transactions": ["ledger_state"],
  *     });
  * 
  *     await holofuel.transactions.ledger_state();
  * })();
+ * </script>
  * ```
  * 
  * Development example
- * ```javascript
- * const HoloWebSDk = require("@holo-host/web-sdk");
- * 
+ * ```html
+ * <script type="text/javascript" src="./holo-web-sdk.js"></script>
+ * <script type="text/javascript">
  * (async () => {
- *     const happ = await HoloWebSDK.init( HoloWebSDK.DEVELOP, "QmUgZ8e6xE1h9fH89CNqAXFQkkKyRh2Ag6jgTNC8wcoNYS" );
- *     await happ.setAgentId( "HcSCj5I6otz4bPt334BF4HeoQp6V9jjsiTfzokneQ7yrv3wiUcKAbrb5D79nt5i" );
+ *     const happ = await HoloWebSDK.init( HoloWebSDK.DEVELOP );
+ * 
  *     const holofuel = await happ.dna( "holofuel", {
  *         "transactions": ["ledger_state"],
  *     });
  * 
  *     await holofuel.transactions.ledger_state();
  * })();
+ * </script>
  * ```
+ * 
+ * @property {symbol} DEVELOP	- Use for setting development mode
+ * @property {symbol} PRODUCT	- Use for setting production mode
  */
 
 /**
- * Constants
- * 
- * @namespace HoloWebSDK
- * 
- * @property {number} DEVELOP	- Use for setting development mode
- * @property {number} PRODUCT	- Use for setting production mode
+ * @global
  */
-const HoloWebSDK			= {};
-define_readonly( HoloWebSDK, "DEVELOP", ModeTypes.Development );
-define_readonly( HoloWebSDK, "PRODUCT", ModeTypes.Production );
+const HoloWebSDK			= {
+    /**
+     * Create and await initiation of a new hApp instance.
+     * 
+     * @async
+     * @function init
+     * 
+     * @example
+     * await HoloWebSDK.init();
+     * 
+     * @example <caption>Equivalent to</caption>
+     * await (new HoloWebSDK.hApp()).init();
+     */
+    async init ( mode ?: symbol, happ_id ?: string ) {
+	const instance			= new hApp( mode, happ_id );
+
+	await instance.init();
+
+	return instance;
+    }
+};
+define_readonly( HoloWebSDK, "DEVELOP", Symbol("DEVELOP") );
+define_readonly( HoloWebSDK, "PRODUCT", Symbol("PRODUCT") );
+
 
 class hApp {
 
-    public mode		: ModeTypes;
+    public mode		: symbol;
     public happ_id	: string;
     
     /**
-     * Initialize hApp instance.  In PRODUCT mode it will get the hApp ID based on the domain.
+     * Initialize hApp instance.
+     * 
+     * **Behind the scenes**
+     * 
+     * In `PRODUCT` mode, you don't have to provide the HHA ID because it will be fetched from
+     * Resolver based on the domain name.
+     * 
+     * The Secure iFrame derives an instance name using the Agent ID, HHA ID, and DNA Alias.  This
+     * is the minimal information required to ensure unique instance IDs for HPOS.  When testing
+     * against Conductor, Agent ID is omitted because HHA ID and DNA Alias are unique.
+     * 
+     * **Supported environments**
+     * 
+     * Holo Hosting
+     *   - Fetches HHA ID from resolver based on domain
+     *   - Creates a random Agent ID for anonymous requests
+     * 
+     * Development mode for Holo Hosting target (talks directly to Conductor)
+     *   - HHA ID must be specified and is used as instance ID prefix
+     *   - Agent ID is managed by Conductor (technically, user is always signed in)
+     * 
+     * Holochain Conductor
+     *   - hApp ID must be specified and is used as instance ID prefix
+     *   - Agent ID is managed by Conductor
+     * 
      * 
      * @class hApp
      * 
@@ -88,9 +129,20 @@ class hApp {
      * 
      * @throws Will throw error if `happ_id` is given when `this.mode` is not set to DEVELOP
      */
-    constructor ( mode : ModeTypes, happ_id : string ) {
+    constructor ( mode ?: symbol, happ_id ?: string ) {
 	this.mode			= mode;
 	this.happ_id			= happ_id;
+    }
+
+    /**
+     * Initiate hApp ID detection
+     * 
+     * @async
+     * 
+     * @example
+     * await happ.init();
+     */
+    public async init () : Promise<void> {
     }
 
     /**
@@ -136,19 +188,20 @@ class hApp {
 	return new DNA( this, alias, config );
     }
 
-    /**
-     * Manually set Agent ID in development mode
-     * 
-     * @param {string} id		- Agent ID set up in Conductor config
-     * 
-     * @throws Will throw error if `this.mode` is not set to DEVELOP
-     * 
-     * @example
-     * const happ = await HoloWebSDK.init( HoloWebSDK.DEVELOP, "QmUgZ8e6xE1h9fH89CNqAXFQkkKyRh2Ag6jgTNC8wcoNYS" );
-     * await happ.setAgentId( "HcSCj5I6otz4bPt334BF4HeoQp6V9jjsiTfzokneQ7yrv3wiUcKAbrb5D79nt5i" );
-     */
-    public setAgentId ( id : string ) : void {
-    }
+    // /**
+    //  * Manually set Agent ID in development mode
+    //  * 
+    //  * @async
+    //  * @param {string} id		- Agent ID set up in Conductor config
+    //  * 
+    //  * @throws Will throw error if `this.mode` is not set to DEVELOP
+    //  * 
+    //  * @example
+    //  * const happ = await HoloWebSDK.init( HoloWebSDK.DEVELOP, "QmUgZ8e6xE1h9fH89CNqAXFQkkKyRh2Ag6jgTNC8wcoNYS" );
+    //  * await happ.setAgentId( "HcSCj5I6otz4bPt334BF4HeoQp6V9jjsiTfzokneQ7yrv3wiUcKAbrb5D79nt5i" );
+    //  */
+    // public async setAgentId ( id : string ) : Promise<void> {
+    // }
 
 }
 
@@ -168,6 +221,8 @@ class DNA {
 
     /**
      * Execute a zome/function for this DNA
+     * 
+     * @async
      * 
      * @param {string} zome		- Zome name
      * @param {string} func		- Function name
